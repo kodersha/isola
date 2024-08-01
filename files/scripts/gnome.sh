dc11301cb2c2a36f614d19ef006cd00cb67aa7e9
Alexandra Travkina <kodersha@yandex.ru>
2024-08-01 16:28:07 +0700

    Update

--- files/scripts/gnome.sh
+++ files/scripts/gnome.sh
@@ -3,9 +3,8 @@ set -oue pipefail
 
 # Ensure machine-id exists and is correct
 if [ ! -s /etc/machine-id ]; then
-  dbus-uuidgen --ensure=/etc/machine-id
+  uuidgen | tr -d '-' > /etc/machine-id
 fi
-dbus-uuidgen --ensure
 
 # GTK
 gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-Dark-solid-alt-blue'
