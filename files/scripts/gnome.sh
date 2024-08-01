5fae17d7fab9d149a0b0996360ddd4893c382272
Alexandra Travkina <kodersha@yandex.ru>
2024-08-01 16:46:06 +0700

    Update

--- files/scripts/gnome.sh
+++ files/scripts/gnome.sh
@@ -6,6 +6,11 @@ if [ ! -s /etc/machine-id ]; then
   uuidgen | tr -d '-' > /etc/machine-id
 fi
 
+# Run the gsettings commands within a dbus-run-session
+dbus-run-session -- bash <<EOF
+
 # GTK
 gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-Dark-solid-alt-blue'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+EOF
