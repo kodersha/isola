f34d43bd0cf5f44c4247e094b858963ce9af3494
Alexandra Travkina <kodersha@yandex.ru>
2024-08-01 16:02:02 +0700

    Update

--- files/scripts/gnome.sh
+++ files/scripts/gnome.sh
@@ -5,10 +5,5 @@ sudo dbus-uuidgen --ensure=/etc/machine-id
 sudo dbus-uuidgen --ensure
 
 # GTK
-gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-Dark-solid-alt-blue"
-
-# Icons
-# gsettings set org.gnome.desktop.interface icon-theme "WhiteSur-Dark"
-
-# Shell
-gsettings set org.gnome.shell.extensions.user-theme name "WhiteSur-Dark-solid-alt-blue"
+gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-Dark-solid-alt-blue'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
