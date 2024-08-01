09ec3dfb4ce63d74328cf0447ada45c8b27e96fb
Alexandra Travkina <kodersha@yandex.ru>
2024-08-01 15:59:55 +0700

    Update

--- files/scripts/gnome.sh
+++ files/scripts/gnome.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+sudo dbus-uuidgen --ensure=/etc/machine-id
+sudo dbus-uuidgen --ensure
+
 # GTK
 gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-Dark-solid-alt-blue"
 
